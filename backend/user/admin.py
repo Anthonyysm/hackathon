f684b8e79f2a322c1ad319a923a69575dfe1a29c
KaykySiq <josekayky@hotmail.com>
2025-08-26 04:11:31 -0300

    feat: Adiciona o CRUD de usuário.

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from user.models import CustomUser, Session
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'password', 'name', 'type', 'birth', 'phone', 'crp')
+    search_fields = ('username', 'email', 'name')
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'start_time', 'end_time', 'psychologist', 'user')
+    search_fields = ('psychologist__username', 'user__username', 'date')    
+
+
+admin.site.register(CustomUser)
+admin.site.register(Session, SessionAdmin)
